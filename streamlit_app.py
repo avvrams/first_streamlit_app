5456b687cb205aa7fa5541169379b0d892896ea7
avvrams <62839361+avvrams@users.noreply.github.com>
2023-06-26 17:19:37 +0100

    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('TEST App')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale and spinach')
+streamlit.text('hard boiled egg')
