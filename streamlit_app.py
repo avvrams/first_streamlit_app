18963e0f6dcc7efb9262abb3245e7d9c8f757668
avvrams <62839361+avvrams@users.noreply.github.com>
2023-06-26 17:21:14 +0100

    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 streamlit.title('TEST App')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale and spinach')
-streamlit.text('hard boiled egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 kale and spinach')
+streamlit.text('🐔 hard boiled egg')
+streamlit.text('🥑🍞 Avocado Toast')
